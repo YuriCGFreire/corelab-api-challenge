0d0a161f875961ef57e8325631676fba4186f61b
YuriCGFreire <yurifreireg@gmail.com>
2022-07-06 18:41:43 -0300

    cars.controller created

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,7 +1,40 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { CarsService } from './cars.service';
+import { CreateCarDTO } from './dto/create-car.dto';
+import { UpdateCarDTO } from './dto/update-car.dto';
 
 @Controller('cars')
 export class CarsController {
   constructor(private readonly carsService: CarsService) {}
+
+  @Post()
+  async createCar(@Body() createCarDTO: CreateCarDTO){
+    return await this.carsService.createCar(createCarDTO)
+  }
+
+  @Get()
+  async findAll(){
+    return await this.carsService.findAll()
+  }
+
+  @Get(":id")
+  async findOne(@Param('id') id: string){
+    return await this.carsService.findOne(id)
+  }
+
+  @Patch(':id')
+  async updateCar(@Param('id') id: string, @Body() updateCarDTO: UpdateCarDTO){
+    return await this.carsService.updateCar(id, updateCarDTO)
+  }
+
+  @Delete(':id')
+  async deleteCar(@Param('id') id: string){
+    return await this.carsService.deleteCar(id)
+  }
+
+  @Patch('isfavorite/:id')
+  async setFavorie(@Param('id') id: string){
+    console.log(id)
+    return await this.carsService.setFavorite(id)
+  }
 }
